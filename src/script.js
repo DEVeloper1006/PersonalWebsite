18d019d0e03d3c271199fd72d6aa44baf41eb79a
DEVeloper <devmody1268@gmail.com>
2023-05-06 13:46:52 -0400

    Finished Project:

--- src/script.js
+++ src/script.js
@@ -1,22 +1,31 @@
 function sendMail (){
-    var params = {
-        name : document.getElementById('name').value,
-        email : document.getElementById('email').value,
-        subject : document.getElementById('subject').value,
-        message : document.getElementById('message').value
-    };
-    const serviceID = 'service_hic82lw';
-    const templateID = 'template_m59mylj';
 
-    emailjs.send(serviceID, templateID, params).then(res=>{
-    document.getElementById('name').value='';
-    document.getElementById('email').value='';
-    document.getElementById('subject').value='';
-    document.getElementById('message').value='';
-    console.log(res);
-    alert('your message sent successfully');
-    })
-    .catch(err=>console.log(err));
+    if (isEmailCorrect() == true) {
+        var params = {
+            name : document.getElementById('name').value,
+            email : document.getElementById('email').value,
+            subject : document.getElementById('subject').value,
+            message : document.getElementById('message').value
+        };
+        const serviceID = 'service_hic82lw';
+        const templateID = 'template_m59mylj';
+    
+        emailjs.send(serviceID, templateID, params).then(res=>{
+        document.getElementById('name').value='';
+        document.getElementById('email').value='';
+        document.getElementById('subject').value='';
+        document.getElementById('message').value='';
+        console.log(res);
+        alert('your message sent successfully');
+        })
+        .catch(err=>console.log(err));
+    } else {
+        alert("Error. Invalid Email");
+        document.getElementById('name').value='';
+        document.getElementById('email').value='';
+        document.getElementById('subject').value='';
+        document.getElementById('message').value='';
+    }
 }
 
 function reset (){
@@ -25,4 +34,14 @@ function reset (){
     document.getElementById('subject').value = "";
     document.getElementById('message').value = "";
 
+}
+
+function isEmailCorrect (){
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (document.getElementById('email').value.match(validRegex)) {      
+        return true;
+    } else {
+        return false;
+      
+    }     
 }
